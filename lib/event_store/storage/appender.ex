9c96c25194f1645ef12f5b5b9773ecca863686ee
Ben Smith <ben@10consulting.com>
2017-07-24 13:29:50 +0100

    Binary format copy from STDIN

--- lib/event_store/storage/appender.ex
+++ lib/event_store/storage/appender.ex
@@ -18,13 +18,20 @@ defmodule EventStore.Storage.Appender do
     |> handle_response(stream_id, events)
   end
 
+  @header ["PGCOPY\n\xff\r\n\0\0\0\0\0\0\0\0\0\0"]
+  @trailer [<<-1 :: signed-16>>]
+
   defp stream_events_into_table(conn, stream_id, events) do
     try do
       Postgrex.transaction(conn, fn (conn) ->
-        stream = Postgrex.stream(conn, "COPY events(event_id, stream_id, stream_version, event_type, correlation_id, causation_id, data, metadata, created_at) FROM STDIN encoding 'UTF8'", [], max_rows: 0)
+        stream = Postgrex.stream(conn, "COPY events(event_id, stream_id, stream_version, event_type, correlation_id, causation_id, data, metadata, created_at) FROM STDIN (FORMAT BINARY)", [], max_rows: 0)
 
-        events
-        |> Stream.map(&to_row/1)
+        encoded_events =
+          events
+          |> Stream.map(&encode/1)
+
+        [@header, encoded_events, @trailer]
+        |> Stream.concat()
         |> Enum.into(stream)
       end)
     rescue
@@ -32,18 +39,27 @@ defmodule EventStore.Storage.Appender do
     end
   end
 
-  defp to_row(event) do
+  defp encode(event) do
     [
-      Integer.to_string(event.event_id), ?\t,
-      Integer.to_string(event.stream_id), ?\t,
-      Integer.to_string(event.stream_version), ?\t,
-      to_string(event.event_type), ?\t,
-      to_string(event.correlation_id), ?\t,
-      to_string(event.causation_id), ?\t,
-      to_string(event.data), ?\t,
-      to_string(event.metadata), ?\t,
-      to_string(event.created_at), ?\n,
+      <<9>>,
+      <<8 :: signed-32, event.event_id :: signed-64>>,
+      <<8 :: signed-32, event.stream_id :: signed-64>>,
+      <<8 :: signed-32, event.stream_version :: signed-64>>,
+      <<byte_size(event.event_type) :: signed-32>>, event.event_type,
+      <<byte_size(event.correlation_id) :: signed-32>>, event.correlation_id,
+      <<byte_size(event.causation_id) :: signed-32>>, event.correlation_id,
+      <<byte_size(event.data) :: signed-32>>, event.data,
+      <<byte_size(event.metadata) :: signed-32>>, event.metadata,
+      encode_date(event.created_at),
     ]
+  end
+
+  @gs_epoch :calendar.datetime_to_gregorian_seconds({{2000, 1, 1}, {0, 0, 0}})
+
+  defp encode_date(%NaiveDateTime{year: year, month: month, day: day, hour: hour, minute: min, second: sec, microsecond: {usec, _}}) do
+    datetime = {{year, month, day}, {hour, min, sec}}
+    secs = :calendar.datetime_to_gregorian_seconds(datetime) - @gs_epoch
+    <<8 :: signed-32, secs * 1_000_000 + usec :: signed-64>>
   end
 
   defp handle_response({:ok, %Postgrex.Stream{}}, stream_id, events) do
